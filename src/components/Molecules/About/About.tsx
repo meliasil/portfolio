e45d363cd90f8d82fa6094d85b58c40a841f7ba9
Your Name <you@example.com>
2024-10-17 10:05:17 +0200

    feat: fix deploy

--- src/components/Molecules/About/About.tsx
+++ src/components/Molecules/About/About.tsx
@@ -5,12 +5,10 @@ const About: React.FC = () => {
   const [animate, setAnimate] = useState(false);
   const sectionRef = useRef<HTMLDivElement | null>(null);
 
-  
   const handleScroll = () => {
     const sectionTop = sectionRef.current?.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
 
-    
     if (sectionTop && sectionTop < windowHeight * 0.8) {
       setAnimate(true);
     }
@@ -18,8 +16,7 @@ const About: React.FC = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-    
-    
+
     handleScroll();
 
     return () => {
@@ -35,14 +32,14 @@ const About: React.FC = () => {
     >
       <h2>About</h2>
       <p>
-        Hello! I'm Silvia Melia, a passionate web developer with experience in
+        "Hello! I'm Silvia Melia, a passionate web developer with experience in
         creating responsive and dynamic websites. Philosophy graduate, I decided
         to broaden my horizons by embarking on a journey into coding and
         front-end development. What connects these two worlds? My curiosity and
         the desire to explore different forms of expression. I am also
         passionate about politics and issues related to human rights and gender
         equality. In my free time, I find peace and inspiration in nature and
-        among my beloved olive trees.
+        among my beloved olive trees."
       </p>
     </section>
   );
