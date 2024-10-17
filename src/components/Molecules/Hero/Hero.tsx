f753f531cad950c78bd3fee890d54db359d8326d
Your Name <you@example.com>
2024-10-17 10:03:22 +0200

    feat: fix deploy

--- src/components/Molecules/Hero/Hero.tsx
+++ src/components/Molecules/Hero/Hero.tsx
@@ -1,13 +1,13 @@
-
 import style from "./Hero.module.scss";
 import GeometricShapes from "@/components/Atoms/GeometricShapes/GeometricShapes";
+import Image from "next/image";
 
 const Hero: React.FC = () => {
   return (
     <section className={style.hero}>
       <GeometricShapes />
       <div className={style.title}>
-        <img src="./foto.jpg" alt="Silvia Melia" />
+        <Image src="./foto.jpg" alt="Silvia Melia" />
         <h1>Silvia Melia</h1>
       </div>
       <div className={style.menu}>
@@ -20,4 +20,3 @@ const Hero: React.FC = () => {
 };
 
 export default Hero;
-
